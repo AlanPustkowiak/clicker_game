d421ba6a5c252044f9d233b47c5093133810681e
Alan Pustkowiak <alan.pustkowiak@outlook.com>
2025-06-30 23:18:11 +0200

    first functionalities

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'clicker_game';
+
+  clickCount = signal<number>(0);
+
+  protected onClick(): void {
+    this.clickCount.update(count => count + 1);
+  }
 }
