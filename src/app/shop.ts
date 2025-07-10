16567dc8d9b65e4cefd8f03c272de324a15ac4d3
Alan Pustkowiak <alan.pustkowiak@outlook.com>
2025-07-10 23:47:06 +0200

    hidding purchased upgrades

--- src/app/shop.ts
+++ src/app/shop.ts
@@ -16,7 +16,8 @@ export interface Upgrade {
       <h3 class="text-2xl font-bold mb-4">Sklep</h3>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         @for (upgrades of upgrades(); track upgrades.id){
-          <div class="border border-gray-300 rounded-lg p-4 bg-white shadow-md">
+          @if (!upgrades.purchased){
+          <div class="border border-gray-300 rounded-lg p-4 bg-white shadow-md" visibility="upgrades.purchased ? 'hidden' : 'visible'">
             <h4 class="font-semibold">{{upgrades.name}}</h4>
             <p class="text-sm text-gray-600 mb-2">{{upgrades.description}}></p>
             <p class="text-lg font-bold text-green-600">{{upgrades.cost}} punktów</p>
@@ -24,6 +25,7 @@ export interface Upgrade {
               {{ upgrades.purchased ? 'kupione' : 'Kup' }}
             </button>
           </div>
+          }
         }
       </div>
     </div>
